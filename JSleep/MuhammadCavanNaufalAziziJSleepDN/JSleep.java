1952e8a19586e85849b6d62fc6e792f7b6e46ad3
Muhammad Cavan Naufal Azizi <mcavannazizi@gmail.com>
2022-09-13 17:52:04 +0700

    Case Study Modul 1

--- JSleep/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
+++ JSleep/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
@@ -1,35 +1,78 @@
 package MuhammadCavanNaufalAziziJSleepDN;
 
-
-/**
- * Write a description of class JSleep here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class JSleep
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    
+    public static float persendis, hargadiskon, harga_awal, persen_admin_fee, admin_fee, result;
+    public static void main(String[] args) {
 
-    /**
-     * Constructor for objects of class JSleep
-     */
-    public JSleep()
+        JSleep hotel = new JSleep();
+        System.out.println("Nama Hotel : " + hotel.getHotelName());
+        System.out.println("ID Hotel : " + hotel.getHotelId());
+        System.out.println("Persentase Diskon : " + hotel.getDiscountPercentage(100000, 90000));
+        System.out.println("Harga Diskon : " + hotel.getDiscountedPrice(100000, 0.1f));
+        System.out.println("Harga Awal : " + hotel.getOriginalPrice(90000, 0.1f));
+        System.out.println("Biaya Admin : " + hotel.getAdminFee(100000));
+        System.out.println("Harga Total : " + hotel.getTotalPrice(100000, 2));
+    }
+    
+
+    public int getHotelId()
     {
-        // initialise instance variables
-        x = 0;
+        return 0;        
+    }
+    
+    public String getHotelName()
+    {
+        return "hotel";        
+    }
+    
+    public boolean isDiscount()
+    {
+        return true;
+    }
+    
+    public float getDiscountPercentage(int beforeDiscount, int afterDiscount) 
+    {   
+        if(beforeDiscount > afterDiscount){
+            persendis = 100;
+        }else{
+            persendis = (float) ((beforeDiscount - afterDiscount) / beforeDiscount);
+        }
+        return persendis;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public float getDiscountedPrice(int price, float discountPercentage)
     {
-        // put your code here
-        return x + y;
+        if(discountPercentage > 100.0f){
+            hargadiskon = 100.0f;
+        }else{
+            hargadiskon = (float) (price - (price * (discountPercentage/100.0f)));
+        }
+        return hargadiskon;
+    }
+    
+    public float getOriginalPrice(int discountedPrice, float discountPercentage)
+    {   
+        harga_awal = (float) (discountedPrice /  (100.0f - discountPercentage));
+        return harga_awal;
+    }
+    
+    public float getAdminFeePercentage()
+    {
+        return persen_admin_fee = 0.05f;
+    }
+    
+    public float getAdminFee(int price)
+    {
+        admin_fee = price * (getAdminFeePercentage());
+        return admin_fee;
+    }
+    
+    public float getTotalPrice(int price, int numberOfNight)
+    {
+        result = ((float) price * numberOfNight) + getAdminFee(price * numberOfNight);
+        return result;
     }
 }
+
