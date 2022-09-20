fcb67b9529fceedccc3bbfec8ce0997f4c752778
Muhammad Cavan Naufal Azizi <mcavannazizi@gmail.com>
2022-09-20 11:17:39 +0700

    Case Study 1

--- JSleep/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
+++ JSleep/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
@@ -6,40 +6,44 @@ public class JSleep
 
     }
 
-    
+
     public static void main(String[] args) {
 
         JSleep hotel = new JSleep();
         System.out.println("Nama Hotel : " + hotel.getHotelName());
         System.out.println("ID Hotel : " + hotel.getHotelId());
-        System.out.println("Persentase Diskon : " + hotel.getDiscountPercentage(1000, 900));
-        System.out.println("Harga Diskon : " + hotel.getDiscountedPrice(1000, 10.0f));
-        System.out.println("Harga Awal : " + hotel.getOriginalPrice(900, 10.0f));
-        System.out.println("Biaya Admin : " + hotel.getAdminFee(1000));
-        System.out.println("Harga Total : " + hotel.getTotalPrice(5000, 1));
+        System.out.println("is Discount : " + hotel.isDiscount());
+        System.out.println("Persentase Diskon : " + hotel.getDiscountPercentage(0, 0));
+        System.out.println("Harga Diskon : " + hotel.getDiscountedPrice(0, 0));
+        System.out.println("Harga Awal : " + hotel.getOriginalPrice(0, 0));
+        System.out.println("Persentase Biaya Admin : " + hotel.getAdminFeePercentage());
+        System.out.println("Biaya Admin : " + hotel.getAdminFee(0));
+        System.out.println("Harga Total : " + hotel.getTotalPrice(0, 0));
     }
-    
+
 
     public static int getHotelId()
     {
-        return 0;        
+        return 0;
     }
-    
+
     public static String getHotelName()
     {
-        return "hotel";        
+        return "hotel";
     }
-    
+
     public static boolean isDiscount()
     {
         return true;
     }
-    
-    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount) 
-    {   
+
+    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount)
+    {
         float persendis;
         if(beforeDiscount < afterDiscount){
             persendis = 0.0f;
+        }else if (beforeDiscount == 0){
+            persendis = 0;
         }else{
             persendis = (float)((beforeDiscount - afterDiscount) / (float)beforeDiscount) * 100.0f;
         }
@@ -47,7 +51,7 @@ public class JSleep
     }
 
     public static int getDiscountedPrice(int price, float discountPercentage)
-    {   
+    {
         int hargadiskon;
         if(discountPercentage > 100.0f){
             hargadiskon = 100;
@@ -56,27 +60,26 @@ public class JSleep
         }
         return hargadiskon;
     }
-    
+
     public static int getOriginalPrice(int discountedPrice, float discountPercentage)
-    {   
+    {
         int harga_awal;
-        harga_awal = (discountedPrice /  (int)(1 - discountPercentage/100));
+        harga_awal = 100 * discountedPrice / (100-(int)discountPercentage);
         return harga_awal;
     }
-    
+
     public static float getAdminFeePercentage()
     {
-        float persen_admin_fee;
-        return persen_admin_fee = 0.05f;
+        return 0.05f;
     }
-    
+
     public static int getAdminFee(int price)
     {
         int admin_fee;
         admin_fee = (int)((float)price * getAdminFeePercentage());
         return admin_fee;
     }
-    
+
     public static int getTotalPrice(int price, int numberOfNight)
     {
         int result;
