a1452d104e7c2b5f0c67c3240d0e025bcc2708c4
Muhammad Cavan Naufal Azizi <mcavannazizi@gmail.com>
2022-10-30 16:43:21 +0700

    TP Modul 6

--- src/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
+++ src/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
@@ -1,6 +1,10 @@
 package MuhammadCavanNaufalAziziJSleepDN;
-import java.sql.Date;
-import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 /**
  * Write a description of class JSleep here.
@@ -11,44 +15,29 @@ import java.util.ArrayList;
 
 public class JSleep
 {
+    class Country
+    {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String[] args)
     {
-        ArrayList<Room> RoomSerialized = new ArrayList<Room>();
-
-        for(int i = 0; i < 5; i++){
-            RoomSerialized.add(i, JSleep.createRoom());
-            System.out.println(RoomSerialized.get(i).toString());
+        String filepath = "D:\\Universitas Indonesia\\Semester 3\\OOP\\Praktikum\\JSleep\\src\\MuhammadCavanNaufalAziziJSleepDN\\city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name : " + input.name);
+            System.out.println("population : " + input.population);
+            System.out.println("states : ");
+            input.listOfStates.forEach(state -> System.out.println(state));
         }
-//        Room RoomA = JSleep.createRoom();
-//        Room RoomB = JSleep.createRoom();
-//
-//        System.out.println("Membuat booking dari tanggal 15 hingga 18");
-//        Date start = Date.valueOf("2022-8-15");
-//        Date end = Date.valueOf("2022-8-20");
-//        System.out.println(Payment.makeBooking(start, end, RoomA));
-//
-//        System.out.println("Membuat booking dari tanggal 15 hingga 18");
-//        Date start2 = Date.valueOf("2022-8-19");
-//        Date end2 = Date.valueOf("2022-8-23");
-//        System.out.println(Payment.makeBooking(start2, end2, RoomA));
-//
-//        System.out.println("Membuat booking dari tanggal 15 hingga 18 untuk kamar berbeda");
-//        Date start3 = Date.valueOf("2022-8-18");
-//        Date end3 = Date.valueOf("2022-8-20");
-//        System.out.println(Payment.makeBooking(start3, end3, RoomB));
-//
-//        //Soal Bonus
-//        System.out.println("Membuat booking dari tanggal 20 hingga 15");
-//        Date start4 = Date.valueOf("2023-1-1");
-//        Date end4 = Date.valueOf("2022-12-31");
-//        System.out.println(Payment.makeBooking(start4, end4, RoomA));
-    }
-
-    public static Room createRoom()
-    {
-        Price price = new Price (100000, 0.5);
-        Room room = new Room("Hotel", 30, price, Facility.AC, City.DEPOK, "Jalan Margonda Raya");
-        return room;
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
 
