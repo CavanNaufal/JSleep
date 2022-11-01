38c1604933fb3cf3141077f8315226b3f727c347
Muhammad Cavan Naufal Azizi <mcavannazizi@gmail.com>
2022-11-01 17:57:58 +0700

    CS Modul 6

--- src/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
+++ src/MuhammadCavanNaufalAziziJSleepDN/JSleep.java
@@ -34,7 +34,7 @@ public class JSleep
             String filepath = "D:\\Universitas Indonesia\\Semester 3\\OOP\\Praktikum\\JSleep\\src\\json\\randomRoomList.json";
 
             JsonTable<Room> tableRoom = new JsonTable<>(Room.class, filepath);
-            List<Room> filterTableRoom = filterByCity(tableRoom, "medan", 0, 5);
+            List<Room> filterTableRoom = filterByCity(tableRoom, "jakarta", 0, 5);
             filterTableRoom.forEach(room -> System.out.println(room.toString()));
         }
         catch (Throwable t)
@@ -51,18 +51,15 @@ public class JSleep
 
     public static List<Room> filterByCity(List<Room> list, String search, int page, int pageSize)
     {
-        List<Room> resultByCity = new ArrayList<>();
-        int start = page * pageSize;
-        int end = start + pageSize;
-        for (int i = start; i < end; i++)
+        List<Room> result = new ArrayList<>();
+        for (Room room : list)
         {
-            Room room = list.get(i);
             if (room.city.name().toLowerCase().contains(search.toLowerCase()))
             {
-                resultByCity.add(room);
+                result.add(room);
             }
         }
-        return resultByCity;
+        return result.subList(page * pageSize, Math.min((page + 1) * pageSize, result.size()));
     }
 
     public static List<Room> filterByPrice(List<Room> list, double minPrice, double maxPrice)
